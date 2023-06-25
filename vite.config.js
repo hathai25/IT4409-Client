000eb648375e89043c1dad00f29861b55aca07b0
hathai25 <phamhuyhathai@gmail.com>
2023-06-25 21:26:48 +0700

    config proxy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://65.108.79.164:3123/api'
+      '/api': {
+        target: 'http://65.108.79.164:3123/api',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
     }
   },
 })
